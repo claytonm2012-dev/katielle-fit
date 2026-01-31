0a60b28424bf282e543bdd0adf5eb6c5e19db933
claytonm2012-dev <claytonm2012@icloud.com>
2026-01-31 01:01:37 -0300

    Update scriptjs.js

--- scriptjs.js
+++ scriptjs.js
@@ -20,3 +20,16 @@ if (window.location.pathname.includes("dashboard")) {
     window.location.href = "index.html";
   }
 }
+
+function login() {
+  const usuario = document.getElementById("usuario").value;
+  const senha = document.getElementById("senha").value;
+  const erro = document.getElementById("erro");
+
+  if (usuario === "katielle" && senha === "1234") {
+    window.location.href = "dashboard.html";
+  } else {
+    erro.innerText = "Usuário ou senha incorretos";
+    erro.style.color = "red";
+  }
+}
