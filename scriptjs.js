351b49f25e68ddab82bf536a9d7954a6a012f613
claytonm2012-dev <claytonm2012@icloud.com>
2026-01-31 01:02:52 -0300

    Update scriptjs.js

--- scriptjs.js
+++ scriptjs.js
@@ -1,27 +1,4 @@
 function login() {
-  const user = document.getElementById("user").value;
-  const pass = document.getElementById("pass").value;
-
-  if (user === "aluno" && pass === "1234") {
-    localStorage.setItem("logado", "true");
-    window.location.href = "dashboard.html";
-  } else {
-    document.getElementById("error").innerText = "Login inválido";
-  }
-}
-
-function logout() {
-  localStorage.removeItem("logado");
-  window.location.href = "index.html";
-}
-
-if (window.location.pathname.includes("dashboard")) {
-  if (!localStorage.getItem("logado")) {
-    window.location.href = "index.html";
-  }
-}
-
-function login() {
   const usuario = document.getElementById("usuario").value;
   const senha = document.getElementById("senha").value;
   const erro = document.getElementById("erro");
@@ -33,3 +10,5 @@ function login() {
     erro.style.color = "red";
   }
 }
+
+
